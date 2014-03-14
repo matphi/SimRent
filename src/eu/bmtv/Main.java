c770990a94e7a77b2f1cdfede283182e4d6bbbd5
matphi <matthieuphilip@hotmail.com>
2014-03-14 23:46:17 +0100

    Some cleaning.

--- src/eu/bmtv/Main.java
+++ src/eu/bmtv/Main.java
@@ -9,13 +9,11 @@ public class Main {
         simu.revenusNonFonciersDispos = 1000;
         simu.tauxFinancier = 1.5;
         simu.tauxEpargne = 3.0;
-        Bonus b = new Bonus(1, 175000.0);
+        //Bonus b = new Bonus(1, 5000.0);
         //simu.addContrat(b);
         //simu.boni.add(b);
-	    Emprunt e = new Emprunt(528000, 1, 18*12, 2.8, false);
-        //simu.addContrat(e);
-        Dette dette = new Dette(e, 1, false);
-        //simu.dettes.add(dette);
+	    Emprunt e;
+        Dette dette;
         e = new Emprunt(160000, 1, 18*12, 3.4, true);
         simu.addContrat(e);
         dette = new Dette(e, 1, true);
@@ -36,27 +34,6 @@ public class Main {
         simu.duree = 12*18;
         long t0 = System.currentTimeMillis();
         simu.calcule();
-        //System.out.println(e.getLoyer(1) + " " + e.getLoyer(e.duree) + " (" + (System.currentTimeMillis() - t0) + ")");
-        //System.out.println("Coût: " + e.getTotalInterets());
         System.out.println(simu);
-                                                   /*
-        System.out.println(Math.floor(Math.random() * 6));
-        String[] names = new String[] {"Antoine", "Aurélie", "Bénédicte",
-                "Emmanuel", "François","Marie-Jeanne", "Matthieu"};
-        int[] so = new int[] {5, 4, 6, 3, 1, 0, 2};
-        int[] peer = new int[names.length];
-        Set<Integer> allocated = new TreeSet<Integer>();
-        for (int i = 0; i < names.length; i++) {
-            int alea = (int) Math.floor(Math.random() * names.length);
-            while (allocated.contains(alea) || alea == i || alea == so[i]) {
-                alea = (alea + 1) % names.length;
-            }
-            allocated.add(alea);
-            peer[i] = alea;
-        }
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(names[i] + " offre à " + names[peer[i]]);
-        }
-        */
     }
 }
